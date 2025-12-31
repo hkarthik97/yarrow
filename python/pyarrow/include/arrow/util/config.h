3a691e6147d1cb8e66af33da09eb8214e2dfde2b
Karthik H <karthik@Karthiks-MacBook-Pro.local>
2025-12-31 09:55:33 -0500

    new update

--- python/pyarrow/include/arrow/util/config.h
+++ python/pyarrow/include/arrow/util/config.h
@@ -15,23 +15,23 @@
 // specific language governing permissions and limitations
 // under the License.
 
-#define ARROW_VERSION_MAJOR 18
-#define ARROW_VERSION_MINOR 1
+#define ARROW_VERSION_MAJOR 20
+#define ARROW_VERSION_MINOR 0
 #define ARROW_VERSION_PATCH 0
 #define ARROW_VERSION ((ARROW_VERSION_MAJOR * 1000) + ARROW_VERSION_MINOR) * 1000 + ARROW_VERSION_PATCH
 
-#define ARROW_VERSION_STRING "18.1.0"
+#define ARROW_VERSION_STRING "20.0.0"
 
-#define ARROW_SO_VERSION "1801"
-#define ARROW_FULL_SO_VERSION "1801.0.0"
+#define ARROW_SO_VERSION "2000"
+#define ARROW_FULL_SO_VERSION "2000.0.0"
 
-#define ARROW_CXX_COMPILER_ID "AppleClang"
-#define ARROW_CXX_COMPILER_VERSION "15.0.0.15000309"
-#define ARROW_CXX_COMPILER_FLAGS " -fno-aligned-new  -Qunused-arguments -fcolor-diagnostics  -Wall -Wno-unknown-warning-option -Wno-pass-failed "
+#define ARROW_CXX_COMPILER_ID "GNU"
+#define ARROW_CXX_COMPILER_VERSION "10.2.1"
+#define ARROW_CXX_COMPILER_FLAGS " -Wno-noexcept-type -Wno-subobject-linkage  -fdiagnostics-color=always  -Wall -fno-semantic-interposition -msse4.2 "
 
 #define ARROW_BUILD_TYPE "RELEASE"
 
-#define ARROW_PACKAGE_KIND "python-wheel-macos"
+#define ARROW_PACKAGE_KIND "python-wheel-manylinux2014"
 
 #define ARROW_COMPUTE
 #define ARROW_CSV
@@ -41,8 +41,8 @@
 #define ARROW_FLIGHT
 /* #undef ARROW_FLIGHT_SQL */
 #define ARROW_IPC
-/* #undef ARROW_JEMALLOC */
-/* #undef ARROW_JEMALLOC_VENDORED */
+#define ARROW_JEMALLOC
+#define ARROW_JEMALLOC_VENDORED
 #define ARROW_JSON
 #define ARROW_MIMALLOC
 #define ARROW_ORC
@@ -63,7 +63,6 @@
 /* #undef ARROW_WITH_OPENTELEMETRY */
 #define ARROW_WITH_RE2
 #define ARROW_WITH_SNAPPY
-/* #undef ARROW_WITH_UCX */
 #define ARROW_WITH_UTF8PROC
 #define ARROW_WITH_ZLIB
 #define ARROW_WITH_ZSTD
